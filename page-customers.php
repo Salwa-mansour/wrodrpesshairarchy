a138394b514d44d909231c76381ba88002f33050
Salwa-Mansour <alqadasisal@gmail.com>
2021-10-27 09:29:06 +0300

    home page and additional templetes added

---
+++ page-customers.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name:custemer page
+ * 
+ *  * Template Post Type: post, page
+ *
+ * @package cursetheme
+ */
+?>
---
+++ templet-pagelectures.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Template Name: pagelecturs
+ *  * Template Post Type: post, page
+ *
+ * @package cursetheme
+ */
+?>
